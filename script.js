5bb9c8108bca86ae562ed7a52613f2bebf690f6b
nruxshod07 <nezametdinovruxsod@gmail.com>
2023-08-09 16:36:04 +0500

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
 let img = document.querySelector('.iPhone')
-let btn = document.querySelectorAll('.btns')
-
+let btns = document.querySelectorAll('.btn')
+let box = document.querySelector('.colors')
+let color = document.querySelector('.color')
+let main = document.querySelector('main')
 let photo = {
     purple: "./img/purpleiPhone.png",
     gold: "./img/goldiPhone.png",
-    silver:"./img/silveriPhone.png",
-    black:"./img/spaceBlackiPhone.png",
+    silver: "./img/silveriPhone.png",
+    black: "./img/spaceBlackiPhone.png",
 }
 
-btn.forEach((btn) => {
-    btn.onclick = () => {
-        let key = btn.getAttribute('data-btn')
 
+btns.forEach((btn) => {
+    btn.onclick = () => {
+        ; let key = btn.getAttribute('data-btn')
         img.src = photo[key]
+        let clicked = event.target
+        box.addEventListener('click', (event) => {
+            for (let i = 0; i < btns.length; i++) {
+                btns[i].classList.remove('focus'); f
+            }
+            clicked.classList.add('focus')
+        });
+        if (btn.classList.contains('purple') === true) {
+            color.innerHTML = "Color - Deep Purple"
+        } else if (btn.classList.contains('gold') === true) {
+            color.innerHTML = "Color - Gold"
+        } else if (btn.classList.contains('silver') === true) {
+            color.innerHTML = "Color - Silver"
+        } else if (btn.classList.contains('spaceBlack') === true) {
+            color.innerHTML = "Color - Space Black"
+        }
+    }
+    btn.onmouseenter = () => {
+        btn.classList.add('hover')
+        if (btn.classList.contains('purple') === true) {
+            color.innerHTML = "Color - Deep Purple"
+        } else if (btn.classList.contains('gold') === true) {
+            color.innerHTML = "Color - Gold"
+        } else if (btn.classList.contains('silver') === true) {
+            color.innerHTML = "Color - Silver"
+        } else if (btn.classList.contains('spaceBlack') === true) {
+            color.innerHTML = "Color - Space Black"
+        }
+    }
+    btn.onmouseleave = () => {
+        btn.classList.remove('hover')
+        if (btn.classList.contains('purple') === false) {
+            color.innerHTML = "Color - Deep Purple"
+        } else if (btn.classList.contains('gold') === false) {
+            color.innerHTML = "Color - Gold"
+        } else if (btn.classList.contains('silver') === false) {
+            color.innerHTML = "Color - Silver"
+        } else if (btn.classList.contains('spaceBlack') === false) {
+            color.innerHTML = "Color - Space Black"
+        }
     }
 })
